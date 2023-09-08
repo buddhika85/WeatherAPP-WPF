0dbfdf8f5e8d1f24fb9ff7f8139f94aec3046643
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-08 12:31:10 +0530

    added some model classes

---
+++ WeatherAPP/Model/City.cs
@@ -0,0 +1,19 @@
+﻿namespace WeatherAPP.Model
+{
+    public class Area
+    {
+        public string ID { get; set; } = null!;
+        public string? LocalizedName { get; set; }
+    }
+
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; } = null!;
+        public string? Type { get; set; }
+        public int Rank { get; set; }
+        public string? LocalizedName { get; set; }
+        public Area? Country { get; set; }
+        public Area? AdministrativeArea { get; set; }
+    }
+}
