bfd7fe08a60b049a6452d928f6a8da1360453063
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-12 17:16:03 +0530

    added weather window view model

---
+++ WeatherAPP/ViewModel/WeatherWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace WeatherAPP.ViewModel
+{
+    public class WeatherWindowViewModel : INotifyPropertyChanged
+    {
+        private string? _query;
+        public string? Query
+        {
+            get => _query;
+            set
+            {
+                _query = value;
+                OnPropertyChanged(nameof(Query));
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string? propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
