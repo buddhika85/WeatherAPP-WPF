67e3bf0a5bfb7c2b72968399ef9273d3e7851f03
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-13 11:52:20 +0530

    Converters added

---
+++ WeatherAPP/ViewModel/ValueConverters/BoolToRainConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WeatherAPP.ViewModel.ValueConverters
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        private const string IsRaining = "Currently raining";
+        private const string IsNotRaining = "Currently not raining";
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return IsRaining;
+
+            return IsNotRaining;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((string)value).Equals(IsRaining);
+        }
+    }
+}
